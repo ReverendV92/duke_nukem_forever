70806e6c09e53c51a4ab0a7d53c7779654b0d441
ReverendV92 <ReverendV92@users.noreply.github.com>
2023-08-08 02:24:38 -0500

    2023 update

---
+++ .content/duke_nukem_forever-characters-gm/lua/autorun/sh_v92_dnf_characters.lua
@@ -0,0 +1,6 @@
+
+player_manager.AddValidModel( "DNF-DukeNukem", "models/v92/characters/dnf/duke/player.mdl" )
+player_manager.AddValidHands( "DNF-DukeNukem", "models/v92/characters/dnf/arms.mdl", 0, "0" )
+
+player_manager.AddValidModel( "DNF-EDFSoldier", "models/v92/characters/dnf/edf_soldier/player.mdl" )
+player_manager.AddValidHands( "DNF-EDFSoldier", "models/v92/characters/dnf/arms.mdl", 0, "1" )
