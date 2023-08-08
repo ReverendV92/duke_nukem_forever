30375a017743f6b01d469b1b3ae939fab50a47ba
ReverendV92 <ReverendV92@users.noreply.github.com>
2023-08-07 22:51:38 -0500

    initial

---
+++ .content/duke_nukem/lua/autorun/PLY_DNF_EDF_Soldier.lua
@@ -0,0 +1,8 @@
+ 
+-- Duke Nukem Forever EDF Soldier Player Model
+-- Model @ Gearbox
+-- Player @ V92
+-- 20 October 2013
+
+player_manager.AddValidModel("EDFSoldier", "models/jessev92/player/misc/edfsoldier.mdl" )
+player_manager.AddValidHands( "EDFSoldier", "models/weapons/c_arms_combine.mdl", 0, "00000000" )
